302a2badbf6b8dcc8ade5aafcda926af95c1619f
johhny99 <tonanhminh1119@gmail.com>
2020-06-10 10:28:14 +0700

    add Twitter box

--- app.py
+++ app.py
@@ -1,6 +1,50 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, redirect, url_for,request
+from flask import make_response,jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class TwitterBot:
+    def __init__(self,username, password):
+        self.username=username
+        self.password=password
+        self.bot=webdriver.Chrome("../chromedriver_win32/chromedriver.exe")
+
+    def login(self):
+        bot=self.bot
+        bot.get('https://twitter.com/login')
+        time.sleep(3)
+        email=bot.find_element_by_name('session[username_or_email]')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+
+@app.route('/api/getsample', methods=['POST'])
+@cross_origin()
+def getsample():
+    try:
+        dataIn= request.json
+        
+        print((dataIn))
+        
+        # message = None
+        if request.method == 'POST':
+            ed=TwitterBot(dataIn['username'],dataIn['password'])
+            res=ed.login()
+        return "<h1>User "+dataIn['username']+" has login !!!</h1>"
+    except:
+        return "<h1>Opps!! Something went wrong !!!</h1>"
+    
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
